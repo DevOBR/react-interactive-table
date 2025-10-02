13a78309b9af007a731114b76087416fedac5a98
DevOBR <bonilla_rdz@hotmail.com>
2025-10-02 15:46:01 -0600

    Adding table, Sort by Country, types and Delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useEffect, useState } from 'react'
+import { type User } from './types.d'
+const API_URL = 'YOUR API HERE'
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([])
+  const [sortByCountry, setsortByCountry] = useState(false)
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((data) => data.json())
+      .then((data) => setUsers(data.results))
+  }, [])
+
+  const sortedUsers = sortByCountry
+    ? users.toSorted((a: User, b: User) =>
+        a.location.country.localeCompare(b.location.country)
+      )
+    : users
+
+  const handleSortUsers = () => {
+    setsortByCountry(!sortByCountry)
+  }
+
+  const handleDeleteUser = (email: string) => {
+    const newUsersList = users.filter(
+      (x) => x.email.toLowerCase() !== email.toLowerCase()
+    )
+    setUsers(newUsersList)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>React Interactive form</h1>
+      </header>
+      <main>
+        <section>
+          <button onClick={handleSortUsers}>Sort by country</button>
+        </section>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Country</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedUsers &&
+              sortedUsers.map((x: User, index: number) => {
+                const background = index % 2 != 0 ? 'grey' : 'transparent'
+                return (
+                  <tr key={x.email} style={{ background: background }}>
+                    <td>
+                      <img src={x.picture.thumbnail} alt='' />{' '}
+                    </td>
+                    <td> {x.name.first} </td>
+                    <td> {x.gender} </td>
+                    <td> {x.location.country} </td>
+                    <td>
+                      <button onClick={() => handleDeleteUser(x.email)}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+      </main>
     </>
   )
 }
