b4c2b59ef8b64ddae7b98576fc04ba2efda72e35
DevOBR <bonilla_rdz@hotmail.com>
2025-10-02 20:10:54 -0600

    Avoiding running fetch if user is chaing filter by country or the data is the same

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useMemo, type ChangeEvent } from 'react'
 import { type User } from './types.d'
 
 const API_URL = import.meta.env.VITE_API_URL
 function App() {
   const [users, setUsers] = useState<User[]>([])
   const [sortByCountry, setsortByCountry] = useState(false)
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+  const [bgColor, setBgColor] = useState(false)
+
   const usersRef = useRef<User[]>([])
+  const filteredUsersRef = useRef<User[]>([])
 
   useEffect(() => {
     const storeUsers = (result: User[]) => {
@@ -19,11 +23,35 @@ function App() {
       .catch((e) => console.log(e))
   }, [])
 
-  const sortedUsers = sortByCountry
-    ? users.toSorted((a: User, b: User) =>
-        a.location.country.localeCompare(b.location.country)
-      )
-    : users
+  const filteredUsers = useMemo(() => {
+    console.warn('filter')
+
+    const newFilteredUsers = filterCountry
+      ? users.filter((x) =>
+          x.location.country.toLowerCase().includes(filterCountry)
+        )
+      : users
+    const isDiferent =
+      newFilteredUsers.some(
+        (x, index) => x !== filteredUsersRef.current[index]
+      ) || filteredUsersRef.current.length !== newFilteredUsers.length
+
+    if (isDiferent) {
+      filteredUsersRef.current = newFilteredUsers
+      return newFilteredUsers
+    }
+
+    return filteredUsersRef.current
+  }, [users, filterCountry])
+
+  const sortedUsers: User[] = useMemo(() => {
+    console.warn('sorted')
+    return sortByCountry
+      ? filteredUsers.toSorted((a: User, b: User) =>
+          a.location.country.localeCompare(b.location.country)
+        )
+      : filteredUsers
+  }, [sortByCountry, filteredUsers])
 
   const handleSortUsers = () => {
     setsortByCountry(!sortByCountry)
@@ -40,6 +68,14 @@ function App() {
     setUsers(usersRef.current)
   }
 
+  const handleOnCountryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilterCountry(e.currentTarget.value)
+  }
+
+  const handleSetBg = () => {
+    setBgColor(!bgColor)
+  }
+
   return (
     <>
       <header>
@@ -47,8 +83,14 @@ function App() {
       </header>
       <main>
         <section>
+          <button onClick={handleSetBg}>Set color rows</button>
           <button onClick={handleSortUsers}>Sort by country</button>
           <button onClick={handleResetUsers}>Resotre Data</button>
+          <input
+            type='text'
+            placeholder='Enter country'
+            onChange={handleOnCountryChange}
+          />
         </section>
 
         <table>
@@ -64,7 +106,8 @@ function App() {
           <tbody>
             {sortedUsers &&
               sortedUsers.map((x: User, index: number) => {
-                const background = index % 2 != 0 ? 'grey' : 'transparent'
+                const background =
+                  (bgColor && index % 2) != 0 ? 'grey' : 'transparent'
                 return (
                   <tr key={x.email} style={{ background: background }}>
                     <td>
